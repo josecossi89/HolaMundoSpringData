f15b824d6b108435b8894aab32aeb3023e960c10
COSSIGOMEZ <COSSIGOMEZ@192.168.0.33>
2021-03-16 07:32:51 -0300

    Modificacion del dominio `persona`, y agregación de anotaciones

--- src/main/java/com/vzla/jc/domain/Persona.java
+++ src/main/java/com/vzla/jc/domain/Persona.java
@@ -1,10 +1,21 @@
 
 package com.vzla.jc.domain;
 
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.*;
 import lombok.Data;
 
 @Data
-public class Persona {
+@Entity
+@Table(name = "persona")
+public class Persona implements Serializable{
+    
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idPersona;
     private String nombre;
     private String apellido;
     private String email;
