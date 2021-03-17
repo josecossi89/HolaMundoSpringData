73bdc236a3d1346b939e8205a6a454d16661d9b5
COSSIGOMEZ <COSSIGOMEZ@192.168.0.33>
2021-03-17 06:39:27 -0300

    Inyección de la interface PersonaDao

--- src/main/java/com/vzla/jc/web/ControladorInicio.java
+++ src/main/java/com/vzla/jc/web/ControladorInicio.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +14,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Slf4j
 public class ControladorInicio {
 
+    @Autowired
+    private PersonaDao personaDao;
     @GetMapping("/")
     public String inicio(Model model) {
 
